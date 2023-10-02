12640cee8dce8f74664641f3888da3cee5553e0a
colex29 <105381803+colex29@users.noreply.github.com>
2023-10-02 21:07:28 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,5 @@ terraform {
 
 }
 
-provider "yandex" {
-  token     = "y0_AgAAAAAGEPMBAATuwQAAAADeJ6zjsolXi-YTSfWMdlAc_AnMqL7N1X4"
-  cloud_id  = "b1gls3ojh90ki4m9r2k6"
-  folder_id = "b1g3h8pmgcqjp1r19to3"
-  zone      = "ru-central1-a"
+
 }
